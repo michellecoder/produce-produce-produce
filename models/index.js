081089f72aceace729ee28cf3962e98297b8ea35
Michelle Oakley <michellecoder38@gmail.com>
2021-09-28 16:58:02 -0700

    added foreign key and fixed a mispelling

--- models/index.js
+++ models/index.js
@@ -4,21 +4,23 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-Catagory.hasMany(Product, {
+Category.hasMany(Product, {
     foreignKey: 'category_id',
     onDelete: 'SET NULL',
 });
 
 Product.belongsTo(Category, {
-    foreignKey: 'category_id,'
+    foreignKey: 'category_id',
 });
 
 Tag.belongsToMany(Product, {
-    through: ProductTag
+    through: ProductTag,
+    foreignKey: 'tag_id',
 });
 
 Product.belongsToMany(Tag, {
-    through: ProductTag
+    through: ProductTag,
+    foreignKey: 'product_id',
 });
 
 module.exports = {
