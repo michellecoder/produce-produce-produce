af0e9c01644f53d083816f80602ef5ab00832118
Michelle Oakley <michellecoder38@gmail.com>
2021-09-28 17:00:05 -0700

    added async to routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
     try {
         const products = await Product.findAll({ include: [{ model: Category }, { model: Tag }] });
         res.status(200).json(products);
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
     try {
         const products = await Product.findByPk(req.params.id, { include: [{ model: Category }, { model: Tag }] });
         res.status(200).json(products);
@@ -92,12 +92,12 @@ router.put('/:id', (req, res) => {
         })
         .then((updatedProductTags) => res.json(updatedProductTags))
         .catch((err) => {
-            // console.log(err);
+            console.log(err);
             res.status(400).json(err);
         });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
     try {
         const products = await Product.destroy({ where: { id: req.params.id } });
         res.status(200).json(products);
